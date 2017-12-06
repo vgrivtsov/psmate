9d5c27a507848e89fc4ee2fa32dade87b3cf8a77
root <vgrivtsov@gmail.com>
2017-12-06 22:23:17 +1000

    add index.html

--- psmate/urls.py
+++ psmate/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('psmate.apps.resume.urls')),
+    #url(r'^', TemplateView.as_view(template_name='index.html')),
+    url(r'^', include('psmate.apps.resume.urls'))
 ]
 
 
