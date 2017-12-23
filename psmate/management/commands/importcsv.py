3907de2c24e70b5eb47a4c43e85620eedb8c2ee2
root <vgrivtsov@gmail.com>
2017-12-23 21:53:53 +1000

    add command

---
+++ psmate/management/commands/importcsv.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+from psmate.models import Okved
+from psmate.models import Okz
+from psmate.models import Eks
+from psmate.models import Okpdtr
+from psmate.models import Okso
+from psmate.models import Educationalreqs
+from psmate.models import Jobtitles
+from psmate.models import Reqworkexperiences
+from psmate.models import Specialconditions
+from psmate.models import Othercharacts
+from psmate.models import Gtfinfo
+from psmate.models import Tfinfo
+from psmate.models import Psinfo
+from psmate.models import Tf_la
+from psmate.models import Tf_oc
+from psmate.models import Tf_nk
+from psmate.models import Tf_rs
+
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **kwargs):
+        # Since the CSV headers match the model fields,
+        # you only need to provide the file's path
+        Educationalreqs.objects.from_csv('./media/csv/educationalreqs.csv', delimiter=';')        
+        self.stdout.write("Import educationalreqs.csv.csv to Postresql OK")
+        
+        Eks.objects.from_csv('./media/csv/EKS.csv', delimiter=';')        
+        self.stdout.write("Import EKS.csv to Postresql OK")
+        
+        Gtfinfo.objects.from_csv('./media/csv/GTFINFO.csv', delimiter=';')        
+        self.stdout.write("Import GTFINFO.csv to Postresql OK")                
+        
+        Jobtitles.objects.from_csv('./media/csv/jobtitles.csv', delimiter=';')        
+        self.stdout.write("Import jobtitles.csv to Postresql OK")        
+        
+        Okpdtr.objects.from_csv('./media/csv/OKPDTR.csv', delimiter=';')        
+        self.stdout.write("Import OKPDTR.csv to Postresql OK")
+        
+        Okso.objects.from_csv('./media/csv/OKSO.csv', delimiter=';')        
+        self.stdout.write("Import OKSO.csv to Postresql OK")        
+        
+        Okved.objects.from_csv('./media/csv/OKVED.csv', delimiter=';')        
+        self.stdout.write("Import OKVED.csv to Postresql OK")
+        
+        Okz.objects.from_csv('./media/csv/OKZ.csv', delimiter=';')        
+        self.stdout.write("Import OKZ.csv to Postresql OK")        
+        
+        Othercharacts.objects.from_csv('./media/csv/othercharacts.csv', delimiter=';')        
+        self.stdout.write("Import othercharacts.csv to Postresql OK")                
+
+        Psinfo.objects.from_csv('./media/csv/PSINFO.csv', delimiter=';')        
+        self.stdout.write("Import PSINFO.csv to Postresql OK")
+
+        Reqworkexperiences.objects.from_csv('./media/csv/reqworkexperiences.csv', delimiter=';')        
+        self.stdout.write("Import reqworkexperiences.csv to Postresql OK")
+
+        Specialconditions.objects.from_csv('./media/csv/specialconditions.csv', delimiter=';')        
+        self.stdout.write("Import specialconditions.csv to Postresql OK")
+
+        Tf_la.objects.from_csv('./media/csv/tf_la.csv', delimiter=';')        
+        self.stdout.write("Import tf_la.csv to Postresql OK")
+
+        Tf_nk.objects.from_csv('./media/csv/tf_nk.csv', delimiter=';')        
+        self.stdout.write("Import tf_nk.csv to Postresql OK")
+
+        Tf_oc.objects.from_csv('./media/csv/tf_oc.csv', delimiter=';')        
+        self.stdout.write("Import tf_oc.csv to Postresql OK")
+
+        Tf_rs.objects.from_csv('./media/csv/tf_rs.csv', delimiter=';')        
+        self.stdout.write("Import tf_rs.csv to Postresql OK")
+
+        Tfinfo.objects.from_csv('./media/csv/TFINFO.csv', delimiter=';')        
+        self.stdout.write("Import TFINFO.csv to Postresql OK")
+
+  
