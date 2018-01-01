eac2a4b53a33869d6cfc02246129349529a1a9f3
root <vgrivtsov@gmail.com>
2018-01-01 19:39:03 +1000

    ps-search overflow heigth 100

--- psmate/apps/services/urls.py
+++ psmate/apps/services/urls.py
@@ -7,6 +7,7 @@ from . import views
 urlpatterns = [
     url(r'^profstandart-autocomplete/$', views.SearchPsAuto.as_view(), name='profstandart-autocomplete'),
     url(r'^search-profstandart/$', views.SearchPSView.as_view(), name='search-profstandart'),
+    url(r'^generator-cv-resume/$', views.CvEditView.as_view(), name='generator-cv-resume'),
     # url(r'^search-profstandart/$', FilterView.as_view(filterset_class=PSFilter,
     #     template_name='services/search-profstandart.html'), name='search-profstandart'),
 
--- psmate/apps/services/views.py
+++ psmate/apps/services/views.py
@@ -5,7 +5,7 @@ except ImportError:
 
 
 from django.shortcuts import render
-from django.views.generic import FormView
+from django.views.generic import FormView, ListView
 from dal import autocomplete
 from psmate.models import *
 from psmate.apps.services.forms import SearchPsForm
@@ -70,3 +70,52 @@ class SearchPSView(FormView):
         return render(request, self.template_name, {'form': form})
 
 ##################
+
+###CV generator###
+
+class CvEditView(ListView):
+
+    #form_class = SearchPsForm
+    template_name = 'services/generator-cv-resume.html'
+    queryset = User.objects.filter(id=1)
+    success_url = None
+
+    def get_context_data(self, **kwargs):
+        context = super(CvEditView, self).get_context_data(**kwargs)
+        context['user'] = self.queryset
+        context['psinfo'] = Psinfo.objects.filter(psregnum=204)
+        context['eks'] = Eks.objects.filter(psregnum=204)
+        context['jobtitles'] = Jobtitles.objects.filter(psregnum=204)
+        return context
+
+        # form=SearchPsForm
+        # data = self.request.GET.get('nameps', None)
+        # if data != None:
+        #     ps_get = Psinfo.objects.filter(id=data) # id=num of autocomplete element. Need to rewrite to psregnum
+        #     psinfo = ps_get[0]
+        #     psregnum = ps_get[0].psregnum
+        #     tfinfo = Tfinfo.objects.filter(psregnum=psregnum).distinct('codetf') # dinstinct delete doubles
+        #     otfinfo = Gtfinfo.objects.filter(psregnum=psregnum).distinct('codeotf')
+        #     okzinfo = Okz.objects.filter(psregnum=psregnum).distinct('codeokz')
+        #     okvedinfo = Okved.objects.filter(psregnum=psregnum).distinct('codeokved')
+        #     
+        #     mixed_tf_otf = []
+        #     
+        #     # for OTF -TF dividing in table used nex list:
+        #     # [[otf1,[tf1, tf2]],[otf2,[tf1, tf2]]]
+        #     for otf in otfinfo:
+        #         mixed_tf = []
+        #         
+        #         for tf in tfinfo:
+        #             if tf.nameotf == otf.nameotf:
+        #                 mixed_tf.append(tf)
+        #         
+        #         mixed_tf_otf.append([otf, mixed_tf])
+        # 
+        #     return render(request, self.template_name, {'psinfo': psinfo,
+        #                                                 'mixed_tf_otf': mixed_tf_otf,
+        #                                                 'okzinfo': okzinfo,
+        #                                                 'okvedinfo': okvedinfo,
+        #                                                 'ngshow' : 'true', # ng-show for id="showps
+        #                                                 'form': form})
+        #return render(request, self.template_name, {'form': form})
