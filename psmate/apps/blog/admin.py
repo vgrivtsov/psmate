4076ce28b24615d437bfbad3d0b7ae80285a5065
root <vgrivtsov@gmail.com>
2018-02-06 22:02:10 +1000

    tinymce

--- psmate/apps/blog/admin.py
+++ psmate/apps/blog/admin.py
@@ -3,6 +3,12 @@ from psmate.models import News
 
 class BlogAdmin(admin.ModelAdmin):
 
+    class Media:
+        js = (
+            '/static/tinymce/classictinymc.js',
+            '/static/tinymce/tinymce.min.js',
+        )
+
     prepopulated_fields = {'slug': ('title',)}
 
 
