0736bc7362538cd032d3a29c06b4a817932a7426
root <vgrivtsov@gmail.com>
2018-03-07 21:21:22 +1000

    cabinet crud ok

--- psmate/apps/companyservices/urls.py
+++ psmate/apps/companyservices/urls.py
@@ -4,10 +4,11 @@ from . import views
 
 urlpatterns = [
     
-    url(r'^registercompany/$', views.RegisterCompanyFormView.as_view(), name='registercompany'),
+    url(r'^registercompany/$', views.OrgCompanyFormView.as_view(), name='registercompany'),
     #url(r'^organizations-list/$', views.OrgListCabinetView.as_view(), name='organizations-list'),
     url(r'^organization-profile/(?P<id>[-\d]+)/$', views.OrgProfileView.as_view(), name='organization-profile'),
-    url(r'^organization-settings/(?P<id>[-\d]+)/$', views.OrgSettingsView.as_view(), name='organization-settings'),    
+    url(r'^organization-settings/(?P<id>[-\d]+)/$', views.OrgSettingsView.as_view(), name='organization-settings'),
+    url(r'^organization-remove/(?P<id>[-\d]+)/$', views.OrgDeleteView.as_view(), name='organization-remove'),
     url(r'^organization-profile/(?P<id>[-\d]+)/registerdepart/$', views.RegisterDepartFormView.as_view(), name='registerdepart'),
     url(r'^organization-profile/(?P<id>[-\d]+)/editdepart/(?P<pk>[-\d]+)/$', views.DepartSettingsView.as_view(), name='editdepart'),
     url(r'^organization-profile/(?P<id>[-\d]+)/removedepart/(?P<pk>[-\d]+)/$', views.DepartDeleteView.as_view(), name='removedepart'),
