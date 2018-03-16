0c458df86e5d599d6adff3b741082052c72aa8ad
root <vgrivtsov@gmail.com>
2018-03-16 16:27:11 +1000

    org profile oi icons

--- psmate/apps/services/views.py
+++ psmate/apps/services/views.py
@@ -28,8 +28,8 @@ import pytils
 
 class SearchPsAuto(autocomplete.Select2QuerySetView):
      def get_queryset(self):
-         # if not self.request.user.is_authenticated(): 
-         #       return User.objects.none() 
+         # if not self.request.user.is_authenticated():
+         #       return User.objects.none()
         qs = Psinfo.objects.all()
 
         if self.q:
@@ -37,12 +37,12 @@ class SearchPsAuto(autocomplete.Select2QuerySetView):
             qs = qs.filter(nameps__icontains=self.q) # icontains - Case-insensitive
 
         return qs
-    
+
 
 class SearchPSView(FormView):
     model = Psinfo
     form_class = SearchPsForm
-    template_name = 'services/search-profstandart.html'    
+    template_name = 'services/search-profstandart.html'
     success_url = None
 
     def get(self, request, *args, **kwargs):
@@ -50,24 +50,24 @@ class SearchPSView(FormView):
         form=SearchPsForm
         data = self.request.GET.get('nameps', None)
         if data != None:
-            ps_get = Psinfo.objects.filter(id=data) 
+            ps_get = Psinfo.objects.filter(id=data)
             psinfo = ps_get[0]
             tfinfo = Tfinfo.objects.filter(ps_id=psinfo.id).distinct('codetf') # dinstinct delete doubles
             otfinfo = Gtfinfo.objects.filter(ps_id=psinfo.id).distinct('codeotf')
             okzinfo = Okz.objects.filter(ps_id=psinfo.id).distinct('codeokz')
             okvedinfo = Okved.objects.filter(ps_id=psinfo.id).distinct('codeokved')
-            
+
             mixed_tf_otf = []
-            
+
             # for OTF -TF dividing in table used nex list:
             # [[otf1,[tf1, tf2]],[otf2,[tf1, tf2]]]
             for otf in otfinfo:
                 mixed_tf = []
-                
+
                 for tf in tfinfo:
                     if tf.nameotf == otf.nameotf:
                         mixed_tf.append(tf)
-                
+
                 mixed_tf_otf.append([otf, mixed_tf])
 
             return render(request, self.template_name, {'psinfo': psinfo,
@@ -84,10 +84,10 @@ class SearchPSView(FormView):
 
 
 class CvEditView(LoginRequiredMixin, UpdateView):
-    
-    template_name = 'services/generator-cv-resume.html'    
+
+    template_name = 'services/generator-cv-resume.html'
     form_class = CvGenForm
-    
+
     model = User
     # success_url = None
 
@@ -95,7 +95,7 @@ class CvEditView(LoginRequiredMixin, UpdateView):
     def get_object(self, queryset=None):
 
         user = self.request.user
-        
+
         if user.profiles.fl_tlph_mob == None: # if user not set settings
             user.profiles.fl_tlph_mob = ''
         if user.profiles.fl_otch == None:
@@ -103,8 +103,8 @@ class CvEditView(LoginRequiredMixin, UpdateView):
         if user.profiles.fl_pd_date == None:
             user.profiles.fl_pd_date = ''
         if user.profiles.fl_adress_real == None:
-            user.profiles.fl_adress_real = ''            
-            
+            user.profiles.fl_adress_real = ''
+
         return user
 
     def get(self, request, *args, **kwargs):
@@ -113,16 +113,16 @@ class CvEditView(LoginRequiredMixin, UpdateView):
 
     def post(self, request, *args, **kwargs):
         self.object = self.get_object()
-        
+
         return super(CvEditView, self).post(request, *args, **kwargs)
 
     def get_success_url(self):
-        return reverse_lazy('presentation-cv-resume')    
+        return reverse_lazy('presentation-cv-resume')
 
     def form_valid(self, form):
         self.object = self.get_object()
-    
-        return super(CvEditView, self).form_valid(form) 
+
+        return super(CvEditView, self).form_valid(form)
 
 
 class LoadPS(View):
@@ -130,15 +130,15 @@ class LoadPS(View):
     def get(self, request, *args, **kwargs):
 
         data = self.request.GET.get('id', None)
-        
+
         if data != None:
-            ps_get = Psinfo.objects.filter(otraslid_id=data)            
+            ps_get = Psinfo.objects.filter(otraslid_id=data)
             jtresult = []
-            
+
             for ps in ps_get:
-                
+
                 jt_get = Jobtitles.objects.filter(ps_id=ps.id).distinct('jobtitle')
-                
+
                 for jt in jt_get:
 
                     jtresult.append({'id' : jt.id, 'jobtitle' : jt.jobtitle,
@@ -146,27 +146,27 @@ class LoadPS(View):
                                      'nameps' : ps.nameps, 'psregnum' : ps.psregnum,
                                      'ps_id' : ps.id,
                                      'pspurposekind' : ps.pspurposekind})
-  
-            return JsonResponse(jtresult, safe=False) 
+
+            return JsonResponse(jtresult, safe=False)
 
 
 class LoadCompt(View):
 
     def get(self, request, *args, **kwargs):
-        
+
         t0 = time()
 
         data = self.request.GET.getlist('psvars', None)
 
         if data != None:
-            
+
             tf_get_raw = Tfinfo.objects.filter(ps_id=data[0])
 
             otf_get_raw = Jobtitles.objects.filter(id=data[1]) # get OTF by jobtitle
 
             otflist = []
             maintfresult = []
-            
+
             for otf in otf_get_raw:
 
                 otflist.append(otf.gtf_id)
@@ -177,25 +177,25 @@ class LoadCompt(View):
                 nkresult = []
                 rsresult = []
                 ocresult = []
-                
+
                 la_get_raw = Tf_la.objects.filter(tf_id=tf.id)
                 nk_get_raw = Tf_rs.objects.filter(tf_id=tf.id)
                 rs_get_raw = Tf_nk.objects.filter(tf_id=tf.id)
                 oc_get_raw = Tf_oc.objects.filter(tf_id=tf.id)
-               
+
                 for la in la_get_raw:
                     laresult.append({'id' : la.id, 'laboraction' : la.laboraction})
-                    
+
                 for nk in nk_get_raw:
                     nkresult.append({'id' : nk.id, 'necessaryknowledge' : nk.requiredskill}) # !!!Swap with RS becouse rosmintrud edition!!!
-                    
+
                 for rs in rs_get_raw:
-                    rsresult.append({'id' : rs.id, 'requiredskill' : rs.necessaryknowledge}) #  # !!!Swap with NK becouse rosmintrud edition!!!                     
-                    
+                    rsresult.append({'id' : rs.id, 'requiredskill' : rs.necessaryknowledge}) #  # !!!Swap with NK becouse rosmintrud edition!!!
+
                 for oc in oc_get_raw:
-                    ocresult.append({'id' : oc.id, 'othercharacteristic' : oc.othercharacteristic})  
-                 
-                # time test 
+                    ocresult.append({'id' : oc.id, 'othercharacteristic' : oc.othercharacteristic})
+
+                # time test
                 #t1 = time()
                 #time_res = t1 - t0
                 #print(time_res)
@@ -204,7 +204,7 @@ class LoadCompt(View):
 
 
                 tfsel = ''
-                if tf.gtf_id in otflist: 
+                if tf.gtf_id in otflist:
 
                     tfsel = 'card info-color white-text mb-0 py-2 z-depth-2'
 
@@ -217,8 +217,8 @@ class LoadCompt(View):
 
                                      })
 
-            
-            return JsonResponse(maintfresult, safe=False) 
+
+            return JsonResponse(maintfresult, safe=False)
 
 #####################
 
@@ -232,7 +232,7 @@ class LoadCV(View):
 
         usercv = self.request.user.profiles.resume
 
-        return JsonResponse(usercv, safe=False) 
+        return JsonResponse(usercv, safe=False)
 
 
 ###CV Presentation###
@@ -241,7 +241,7 @@ class CvPresentView(ListView):
 
     template_name = 'services/presentation-cv-resume.html'
     model = User
-   
+
     def get_context_data(self, **kwargs):
 
         udata = super(CvPresentView, self).get_context_data(**kwargs)
@@ -254,8 +254,8 @@ class CvPresentView(ListView):
         if user.profiles.fl_pd_date == None:
             user.profiles.fl_pd_date = ''
         if user.profiles.fl_adress_real == None:
-            user.profiles.fl_adress_real = ''             
- 
+            user.profiles.fl_adress_real = ''
+
         cv =user.profiles.resume # get resume - JSONb field
 
         cvcleared = []
@@ -264,33 +264,33 @@ class CvPresentView(ListView):
         for i in cv:
 
             quali = []
-            
+
             for s in i['Quali'] :
-      
-                tf_arr = [] 
-                la_arr = [] 
-                rs_arr = [] 
+
+                tf_arr = []
+                la_arr = []
+                rs_arr = []
                 nk_arr = []
                 oc_arr = []
                 # found and append only selected competences in cv
                 for tf in s['FL_cv_TF']:
-                    
+
                     if key in tf and value == tf[key]:
                         tf_arr.append({'nametf' : tf['nametf'], 'codetf' : tf['codetf']})
                         # append if tf_arr != []
-                         
+
                     for la in tf['laboractions'] :
                         if key in la and value == la[key]:
                             la_arr.append({'laboraction' : la['laboraction']})
-                            
+
                     for rs in tf['requiredskills'] :
                         if key in rs and value == rs[key]:
                             rs_arr.append({'requiredskill' : rs['requiredskill']})
-                            
+
                     for nk in tf['necessaryknowledges'] :
                         if key in nk and value == nk[key]:
-                            nk_arr.append({'necessaryknowledge' : nk['necessaryknowledge']})                            
-                
+                            nk_arr.append({'necessaryknowledge' : nk['necessaryknowledge']})
+
                     for oc in tf['othercharacteristics'] :
                         if key in oc and value == oc[key]:
                             oc_arr.append({'othercharacteristic' : oc['othercharacteristic']})
@@ -303,17 +303,17 @@ class CvPresentView(ListView):
                                'nk' : nk_arr,
                                'oc' : oc_arr
                             })
-            
+
             cvcleared.append(
                             {'companyname' : i['FL_cv_companyName'],
                             'startdate' :   i['FL_cv_WorkStartDate'],
                             'enddate' :     i['FL_cv_WorkEndDate'],
                             'keyskills' :    i['KeySkills'],
                             'quali' : quali})
-                   
 
-            
-        return {'cvcleared' :cvcleared} 
+
+
+        return {'cvcleared' :cvcleared}
 
 
 ###Get competences - search from index.html###
@@ -322,23 +322,23 @@ class CvPresentView(ListView):
 ###Autocompete###
 # class SearchJtAuto(autocomplete.Select2QuerySetView):
 #      def get_queryset(self):
-# 
+#
 #         qs = Jobtitles.objects.all()
-# 
+#
 #         if self.q:
-# 
-# 
+#
+#
 #             qs = qs.filter(jobtitle__icontains=self.q) # icontains - Case-insensitive
-#            
-# 
+#
+#
 #         return qs
-# 
-# 
-# 
+#
+#
+#
 # class SearchJtView(FormView):
 #     model = Jobtitles
 #     form_class = SearchJtForm
-#     template_name = 'index.html'    
+#     template_name = 'index.html'
 #     success_url = None
 
 class SearchJT(FormView):
@@ -365,26 +365,26 @@ class ShowJTlist(ListView):
             Paginate by specified value in querystring, or use default class property value.
             """
             return self.request.GET.get('paginate_by', self.paginate_by)
-        
-       
+
+
     def get_queryset(self,  *args, **kwargs):
-    
+
         search_data = self.request.GET.get('search', None)
-    
+
         jtresult = []
-        
-        if search_data:        
-            
+
+        if search_data:
+
             jt_get = Jobtitles.objects.filter(jobtitle__icontains=search_data).distinct('id')
-        
+
             for jt in jt_get:
-           
+
                 ps = Psinfo.objects.filter(id=jt.ps_id)
-                
+
                 otrasl = ps[0].otraslid
                 ### Slug save
                 jt.slug = pytils.translit.slugify(jt.jobtitle) + '-' + str(jt.id)
-                jt.save()               
+                jt.save()
                 ###
                 jtresult.append({'id' : jt.id, 'jobtitle' : jt.jobtitle, 'nameotf' : jt.nameotf,
                                  'slug' : jt.slug,
@@ -395,16 +395,18 @@ class ShowJTlist(ListView):
                                  },
                     )
 
-            return sorted(jtresult, key=itemgetter('jobtitle')) 
+            return sorted(jtresult, key=itemgetter('jobtitle'))
+        return sorted(jtresult, key=itemgetter('jobtitle'))
 
-        
+
+
 class JTDetailsView(ListView):
 
     template_name = 'services/jobtitle-details.html'
     model = Jobtitles
-    
+
     def get(self, request, *args, **kwargs):
-        
+
         jt_slug =  self.kwargs['slug']
 
         if jt_slug:
@@ -417,7 +419,7 @@ class JTDetailsView(ListView):
             specialconditions = Specialconditions.objects.filter(gtf_id=jt[0].gtf_id)
             othercharacts = Othercharacts.objects.filter(gtf_id=jt[0].gtf_id)
             tfs = Tfinfo.objects.filter(gtf_id=jt[0].gtf_id)
-            
+
             otrasl = ps[0].otraslid
 
             jtresult = {'id' : jt[0].id,
@@ -433,13 +435,13 @@ class JTDetailsView(ListView):
                         'specialconditions' : specialconditions,
                         'othercharacts' : othercharacts,
                         'tfs' : tfs,
-                        
+
                         }
-                
+
 
             return render(request, self.template_name, {'jtresult': jtresult})
 
-    
+
 class OfficialInstructions(ListView):
 
     template_name = 'services/official-instructions.html'
@@ -447,13 +449,13 @@ class OfficialInstructions(ListView):
 
 
     def get(self, request, *args, **kwargs):
-        
+
         t0 = time()
 
-        data =  self.kwargs['slug'] 
-        
+        data =  self.kwargs['slug']
+
         if data != None:
-            
+
             jt = Jobtitles.objects.filter(slug=data)
 
             ps = Psinfo.objects.filter(id=jt[0].ps_id)
@@ -462,31 +464,31 @@ class OfficialInstructions(ListView):
             specialconditions = Specialconditions.objects.filter(gtf_id=jt[0].gtf_id)
             othercharacts = Othercharacts.objects.filter(gtf_id=jt[0].gtf_id)
             tfs = Tfinfo.objects.filter(gtf_id=jt[0].gtf_id)
-          
+
             otrasl = ps[0].otraslid
 
             generaldatas = []
-                        
+
             #make jobtitle Roditelny paezh
 
             jt_rod = []
             morph = pymorphy2.MorphAnalyzer()
-       
+
             #cut non need padezh string beenween '( )'
             pattern = re.compile("[\(\[].*?[\)\]]")
             if re.findall(pattern, jt[0].jobtitle):
                 cuttedstring = re.findall(pattern, jt[0].jobtitle)[0]
             else:
                 cuttedstring = ''
-            
+
             cleared_jt = re.sub("[\(\[].*?[\)\]]", "", jt[0].jobtitle)
-            
+
            # print(cuttedstring)
-            
-            pos_list = ['NOUN', 'ADJF', 'ADJS', 'PRTF', 'PRTS'] 
+
+            pos_list = ['NOUN', 'ADJF', 'ADJS', 'PRTF', 'PRTS']
             case_list = []
 
-            
+
             for i in cleared_jt.split(' '):
                 p = morph.parse(i)[0]
                 print(p)
@@ -495,19 +497,19 @@ class OfficialInstructions(ListView):
 
                     #print(jt_rod_word)
                     if p.inflect({'gent'}) :
-                        
+
                         changed_word = p.inflect({'sing', 'gent'}).word
                         if changed_word == 'риска-менеджера':
                             changed_word = 'риск-менеджера'
                         if changed_word == 'бренда-менеджера':
-                            changed_word = 'бренд-менеджера'                        
+                            changed_word = 'бренд-менеджера'
                         if changed_word == 'брэнда-менеджера':
-                            changed_word = 'брэнд-менеджера'                         
-                        
+                            changed_word = 'брэнд-менеджера'
+
                         jt_rod.append(changed_word)
-                    
+
                     else:
-                        
+
                         jt_rod.append(i)
 
                 else:
@@ -516,9 +518,9 @@ class OfficialInstructions(ListView):
 
            # print(jt_rod)
             jobtitlerod = ' '.join(jt_rod) + ' '+cuttedstring
-            
+
             generaldatas = {
-                
+
                     'jobtitleid' : jt[0].id,
                     'jobtitle' : jt[0].jobtitle,
                     'jobtitlerod' : jobtitlerod,
@@ -533,13 +535,13 @@ class OfficialInstructions(ListView):
                     'othercharacts' : othercharacts,
                     'tfs' : tfs,
                     }
-            
+
             laresult = []
             nkresult = []
             rsresult = []
-            ocresult = []            
+            ocresult = []
             tfresult = []
-            
+
             for tf in tfs: # select TF only for selected jobtitle
 
                 la_get_raw = Tf_la.objects.filter(tf_id=tf.id)
@@ -552,17 +554,17 @@ class OfficialInstructions(ListView):
 
                 for la in la_get_raw:
                     laresult.append({'id' : la.id, 'laboraction' : la.laboraction})
-                    
+
                 for nk in nk_get_raw:
                     nkresult.append({'id' : nk.id, 'necessaryknowledge' : nk.requiredskill}) # !!!Swap with RS becouse rosmintrud edition!!!
-                    
+
                 for rs in rs_get_raw:
-                    rsresult.append({'id' : rs.id, 'requiredskill' : rs.necessaryknowledge}) #  # !!!Swap with NK becouse rosmintrud edition!!!                     
-                    
+                    rsresult.append({'id' : rs.id, 'requiredskill' : rs.necessaryknowledge}) #  # !!!Swap with NK becouse rosmintrud edition!!!
+
                 for oc in oc_get_raw:
-                    ocresult.append({'id' : oc.id, 'othercharacteristic' : oc.othercharacteristic})  
-                 
-                # time test 
+                    ocresult.append({'id' : oc.id, 'othercharacteristic' : oc.othercharacteristic})
+
+                # time test
                 t1 = time()
                 time_res = t1 - t0
                 #print(time_res)
@@ -576,7 +578,7 @@ class OfficialInstructions(ListView):
 
 
 
-            
+
             return render(request, self.template_name, {'generaldatas': generaldatas,
                                                         'requirements' : requirements
                                                         })
