1e99687caa2073f3a42b2873ddc12cda97054148
root <vgrivtsov@gmail.com>
2018-03-21 18:00:45 +1000

    faq about

--- psmate/urls.py
+++ psmate/urls.py
@@ -30,15 +30,17 @@ urlpatterns = [
     url(r'^accounts/signup', SignupView.as_view(success_url='/settings/'), name='account_signup'),
     path('account_login', auth_views.LoginView.as_view(template_name='layouts/layout.html',
                                                success_url='/'), name='account_login'),
-    path('account_logout', auth_views.LogoutView.as_view(template_name='layouts/layout.html'), name='account_logout'),     
+    path('account_logout', auth_views.LogoutView.as_view(template_name='layouts/layout.html'), name='account_logout'),
     url(r'^admin/', admin.site.urls),
     # url(r'^$', TemplateView.as_view(template_name='index.html')),account_signup
     url(r'^agreement/$', TemplateView.as_view(template_name='agreement.html')),
+    url(r'^faq/', TemplateView.as_view(template_name='faq.html'), name='faq'),
+    url(r'^about/', TemplateView.as_view(template_name='about.html'), name='about'),
     url(r'^', include('psmate.apps.resume.urls')),
     url(r'^', include('psmate.apps.usercabinet.urls')),
     url(r'^', include('psmate.apps.services.urls')),
     url(r'^', include('psmate.apps.blog.urls')),
     url(r'^', include('psmate.apps.companyservices.urls')),
     url(r'^ckeditor/', include('ckeditor_uploader.urls')),
-]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
