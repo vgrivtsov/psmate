0f512a022f686296813819fc3e06f5901ae41d4a
vgrivtsov <vgrivtsov@gmail.com>
2019-02-21 16:28:20 +1000

    sql

--- sql/management.sql
+++ sql/management.sql
@@ -5,14 +5,14 @@
 UPDATE psmate_jobtitles A
 SET jobtitle_rod = jt_rod_create(B.jobtitle)
 FROM psmate_jobtitles B
-WHERE A.id = B.id;
+WHERE A.jobtitle_rod IS NULL AND A.id = B.id;
 
 -----------SLUGIFY--------------------
 
 UPDATE psmate_jobtitles A
 SET slug = jt_slug_create(B.jobtitle, B.id)
 FROM psmate_jobtitles B
-WHERE A.id = B.id;
+WHERE A.slug IS NULL AND A.id = B.id;
 
 
 ---------------------------------
@@ -72,7 +72,6 @@ ALTER SEQUENCE psmate_tfinfo_id_seq RESTART WITH 1;
 ALTER SEQUENCE psmate_offinsts_id_seq RESTART WITH 1;
 
 ---------------------------------------------------------------------
-
 UPDATE psmate_gtfinfo
 SET    ps_id = psmate_psinfo.id
 FROM   psmate_psinfo
@@ -176,6 +175,122 @@ WHERE  psmate_gtfinfo.nameotf = psmate_tf_rs.nameotf;
 UPDATE psmate_tf_rs
 SET    tf_id = psmate_tfinfo.id
 FROM   psmate_tfinfo
-WHERE  psmate_tfinfo.nametf = psmate_tf_rs.nametf;
+WHERE psmate_tfinfo.nametf = psmate_tf_rs.nametf;
+---------------------------------------------------------------------
+UPDATE psmate_gtfinfo A
+SET    ps_id = B.id
+FROM   psmate_psinfo B
+WHERE  A.ps_id IS NULL AND B.psregnum = A.psregnum;
 
+UPDATE psmate_tfinfo A
+SET ps_id = B.ps_id, gtf_id = B.id 
+FROM   psmate_gtfinfo B
+WHERE  A.ps_id IS NULL AND A.gtf_id IS NULL AND B.nameotf = A.nameotf;
+
+UPDATE psmate_jobtitles A
+SET    ps_id = B.ps_id, gtf_id = B.id 
+FROM   psmate_gtfinfo B
+WHERE  A.ps_id IS NULL AND A.gtf_id IS NULL AND B.nameotf = A.nameotf;
+
+UPDATE psmate_educationalreqs A
+SET    ps_id = B.ps_id, gtf_id = B.id 
+FROM   psmate_gtfinfo B
+WHERE  A.ps_id IS NULL AND A.gtf_id IS NULL AND B.nameotf = A.nameotf;
+
+UPDATE psmate_eks A
+SET    ps_id = B.ps_id, gtf_id = B.id 
+FROM   psmate_gtfinfo B
+WHERE  A.ps_id IS NULL AND A.gtf_id IS NULL AND B.nameotf = A.nameotf;
+
+UPDATE psmate_okpdtr A
+SET    ps_id = B.ps_id, gtf_id = B.id 
+FROM   psmate_gtfinfo B
+WHERE  A.ps_id IS NULL AND A.gtf_id IS NULL AND B.nameotf = A.nameotf;
+
+UPDATE psmate_okso A
+SET    ps_id = B.ps_id, gtf_id = B.id 
+FROM   psmate_gtfinfo B
+WHERE  A.ps_id IS NULL AND A.gtf_id IS NULL AND B.nameotf = A.nameotf;
+
+UPDATE psmate_okved A
+SET    ps_id = B.id
+FROM   psmate_psinfo B
+WHERE  A.ps_id IS NULL AND B.psregnum = A.psregnum;
+
+UPDATE psmate_okz A
+SET    ps_id = B.ps_id, gtf_id = B.id 
+FROM   psmate_gtfinfo B
+WHERE  A.ps_id IS NULL AND A.gtf_id IS NULL AND B.nameotf = A.nameotf;
+
+UPDATE psmate_okz A
+SET    ps_id = B.ps_id, gtf_id = B.id 
+FROM   psmate_gtfinfo B
+WHERE  A.ps_id IS NULL AND A.gtf_id IS NULL AND B.nameotf = A.nameotf;
+
+UPDATE psmate_othercharacts A
+SET    ps_id = B.ps_id, gtf_id = B.id 
+FROM   psmate_gtfinfo B
+WHERE  A.ps_id IS NULL AND A.gtf_id IS NULL AND B.nameotf = A.nameotf;
+
+UPDATE psmate_reqworkexperiences A
+SET    ps_id = B.ps_id, gtf_id = B.id 
+FROM   psmate_gtfinfo B
+WHERE  A.ps_id IS NULL AND A.gtf_id IS NULL AND B.nameotf = A.nameotf;
+
+UPDATE psmate_specialconditions A
+SET    ps_id = B.ps_id, gtf_id = B.id 
+FROM   psmate_gtfinfo B
+WHERE  A.ps_id IS NULL AND A.gtf_id IS NULL AND B.nameotf = A.nameotf;
+
+UPDATE psmate_tf_la A
+SET    ps_id = B.ps_id, gtf_id = B.id 
+FROM   psmate_gtfinfo B
+WHERE  A.ps_id IS NULL AND A.gtf_id IS NULL AND B.nameotf = A.nameotf;
+
+UPDATE psmate_tf_la A
+SET    tf_id = B.id
+FROM   psmate_tfinfo B
+WHERE  A.tf_id IS NULL AND B.nametf = A.nametf;
+
+UPDATE psmate_tf_nk A
+SET    ps_id = B.ps_id, gtf_id = B.id 
+FROM   psmate_gtfinfo B
+WHERE  A.ps_id IS NULL AND A.gtf_id IS NULL AND B.nameotf = A.nameotf;
+
+UPDATE psmate_tf_nk A
+SET    tf_id = B.id
+FROM   psmate_tfinfo B
+WHERE  A.tf_id IS NULL AND B.nametf = A.nametf;
+
+UPDATE psmate_tf_oc A
+SET    ps_id = B.ps_id, gtf_id = B.id 
+FROM   psmate_gtfinfo B
+WHERE  A.ps_id IS NULL AND A.gtf_id IS NULL AND B.nameotf = A.nameotf;
+
+UPDATE psmate_tf_oc A
+SET    tf_id = B.id
+FROM   psmate_tfinfo B
+WHERE  A.tf_id IS NULL AND B.nametf = A.nametf;
+
+UPDATE psmate_tf_rs A
+SET    ps_id = B.ps_id, gtf_id = B.id 
+FROM   psmate_gtfinfo B
+WHERE  A.ps_id IS NULL AND A.gtf_id IS NULL AND B.nameotf = A.nameotf;
+
+UPDATE psmate_tf_rs A
+SET    tf_id = B.id
+FROM   psmate_tfinfo B
+WHERE  A.tf_id IS NULL AND B.nametf = A.nametf;
+
+----------------------------------------------------------------------
+
+--Find duplicates:
+
+select * from psmate_psinfo A
+where (select count(*) from psmate_psinfo B
+where B.psregnum = A.psregnum) > 1
+
+select * from psmate_jobtitles A
+where (select count(*) from psmate_jobtitles B
+where B.jobtitle = A.jobtitle AND B.psregnum = A.psregnum) > 1
 
